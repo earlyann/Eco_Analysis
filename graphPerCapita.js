3e78473d4afc1121a0d38f84dccdbcf42f1d5116
erica.b.graboyes <erica.b.graboyes@gmail.com>
2023-04-17 18:18:51 -0500

    add summary table table, re-scope functions to components

--- graphPerCapita.js
+++ graphPerCapita.js
@@ -12,24 +12,23 @@ const dropDownYear = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008
 const dropDownContinent = ["AF", "AS", "EU", "NA", "OC", "SA"]
 
 ////////////////////////////////////////////////////t//////////////////////
-function keymetrics() {
+function cleanData() {
     // loop through data to generate list of countries for given year
     const cnty = []
     rawData.forEach(rD => {
         if(!cnty.some(c => c === rD.Country)) {
-            cnty.push(rD.Country)}},
-        // console.log("Country Code", cntyCd)
-    );
+            cnty.push(rD.Country)
+        }
+    });
     
     // loop through data to generate list of categories for given year
     const categories = []    
     rawData.forEach(rD => {
         if(!categories.some(c => c === rD.Category)) {
-            categories.push(rD.Category)}},
-        // console.log("Categories", categories)
-    );
+            categories.push(rD.Category)
+        }
+    });
 
-    const bubblechart = []
     const formatData = []
     // Outer Loop iterates over countries / Inner Loop iterates over Category. for each country returns object with category and value
     cnty.forEach ((c) => {
@@ -45,29 +44,33 @@ function keymetrics() {
                         value: rD.Value
                     });
                     }
-                };
-            },
-        )
+            };
+        })
         
-    formatData.push({
-        country:c,
-        oil: cntyCategories.find(cC => cC.category === 'Oil').value,
-        coal: cntyCategories.find(cC =>cC.category === 'Coal').value, 
-        gas: cntyCategories.find(cC => cC.category === 'Gas').value,
-        cement: cntyCategories.find(cC => cC.category === 'Cement').value, 
-        flaring: cntyCategories.find(cC => cC.category === 'Flaring').value, 
-        other: cntyCategories.find(cC => cC.category === 'Other').value,
-        total: cntyCategories.find(cC => cC.category === 'Total').value,
-        gdp: cntyCategories.find(cC => cC.category === 'GDP per capita (current US$)').value,
-        population: cntyCategories.find(cC => cC.category === 'Population, total').value/1000000,
+        formatData.push({
+            country:c,
+            oil: cntyCategories.find(cC => cC.category === 'Oil').value,
+            coal: cntyCategories.find(cC =>cC.category === 'Coal').value, 
+            gas: cntyCategories.find(cC => cC.category === 'Gas').value,
+            cement: cntyCategories.find(cC => cC.category === 'Cement').value, 
+            flaring: cntyCategories.find(cC => cC.category === 'Flaring').value, 
+            other: cntyCategories.find(cC => cC.category === 'Other').value,
+            total: cntyCategories.find(cC => cC.category === 'Total').value,
+            gdp: cntyCategories.find(cC => cC.category === 'GDP per capita (current US$)').value,
+            population: cntyCategories.find(cC => cC.category === 'Population, total').value/1000000,
+        });
     });
-
+    
     // Sort bargraph data by total emissions for stacked bar chart 
     formatData.sort((d1, d2) => {
-        return d2.total - d1.total})
-    });
+        return d2.total - d1.total});
+
+    return formatData
+};
 
 ///////////////////--STACKED BAR CHART IN APEX--///////////////////////////////
+function plotBarChart (formatData) {
+    console.log("Test FD", formatData)
     var options = {
         series: [ 
             {name: 'Gas', data: (formatData.map(fD => fD.gas)), color: '#4A8DDC'},      
@@ -115,101 +118,128 @@ function keymetrics() {
       
     var chart = new ApexCharts(document.querySelector("#bar"), options);
     chart.render();
-////////////////////////////////////////////////////////////////////////////////////
+}
+//////////////////////--APEX BUBBLE CHART --//////////////////////////////
+function plotBubbleChart (formatData) {
+    let dataBubble = formatData.map(dB => {
+        return {
+            name: dB.country,
+            data: [[
+                dB.total,
+                dB.gdp,
+                dB.population
+            ]]
+        }
+    })
+    
+    let axisLimit = formatData.map(dB => {
+        return {
+            yaxis: dB.gdp
+        }
+    },)
 
-let dataBubble = formatData.map(dB => {
-    return [
-        dB.total,
-        dB.gdp,
-        dB.population
-    ]})
+    // Sort y-axis values to determin max
+    axisLimit.sort((d1, d2) => {
+        return d2.yaxis - d1.yaxis});
 
-    console.log(dataBubble)
-
-// function generateData(baseval, count, yrange) {
-//     var i = 0;
-//     var series = [];
-//     while (i < count) {
-//       series.push([
-//       	Math.floor(Math.random() * (750 - 1 + 1)) + 1, 
-//       	Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min, 
-//       	Math.floor(Math.random() * (75 - 15 + 1)) + 15
-//       ]);
-
-//       console.log("Test", series),
-//       baseval += 86400000;
-//       i++;
-//     }
-//     return series;
-//   }
-
-//   var chart = new ApexCharts(
-//     document.querySelector('#bubble'), {
-//       chart: { height: 350, type: 'bubble', },
-//       dataLabels: { enabled: false },
-//       colors: ["red"],
-//       series: [
-//         { name: 'Bubble1', data: generateData(new Date('11 Feb 2017 GMT').getTime(), 20, { min: 10, max: 60 }) }
-//       ],
-//       fill: { opacity: 0.8 },
-//       title: { text: 'Simple Bubble Chart' },
-//       xaxis: { tickAmount: 12, type: 'category' },
-//       yaxis: { max: 70 }
-//     }
-//   );
-
-//   chart.render();
-
- // declare layout for bubblechart
-
-//  var bubble = [{
-//     x: formatData.map(fD => fD.total),
-//     y: ((formatData.map(fD => fD.gdp))),
-//     text: formatData.map(fD => fD.country),
-//     mode: 'markers',
-//     marker: {
-//         size:25,
-//         sizemode: 'area',
-//         color:  formatData.map(fD => fD.total),
-//         colorscale: 'Portland',
-//         opacity: 0.8},},];
-
-//  var bubbleLayout = {
-//     title: {
-//         text: `Overview of a country's per Capita CO2 emissions & GDP` ,
-//         font: {
-//             family: 'Times New Roman',
-//             size: 18,
-//             color: 'dark gray',
-//             },
-//     },
-//     autosize: true,
-//     responsive: true,
-//     xaxis: {
-//         automargin: true,
-//         title: {
-//             text: 'Total CO2 Emissions per Capita',
-//             font: {
-//                 family: 'Times New Roman',
-//                 size: 14,
-//                 color: 'dark gray',
-//                 },
-//         },
-//     },
-//     yaxis: { 
-//         automargin: true,
-//         title: {
-//             text: 'GDP per Capita',
-//             font: {
-//                 family: 'Times New Roman',
-//                 size: 14,
-//                 color: 'dark gray',
-//                 },
-//         },
-//     },};
-//     Plotly.newPlot('bubble', bubble, bubbleLayout)
+    var bubbleChart = new ApexCharts(
+        document.querySelector('#bubble'), {
+            chart: {type: 'bubble', },
+            dataLabels: {enabled: false },
+            series: dataBubble,
+            fill: { opacity: 0.8 },
+            title: { text: 'Simple Bubble Chart' },
+            xaxis: {tickAmount: 10, type: 'category' },
+            yaxis: {max: (axisLimit[0].yaxis)  }
+        }
+    );
+    bubbleChart.render();
 };
-////////////////////////////////////
+///////////////-- PLOTLY BUBBLECHART -- ///////////////////////////////////
+// function plotSimpleBubble (formatData) {
+    
+//     var bubble = [{
+//         x: formatData.map(fD => fD.total),
+//         y: (formatData.map(fD => fD.gdp)),
+//         text: formatData.map(fD => fD.country),
+//         mode: 'markers',
+//         marker: {
+//             size:formatData.map(fD => fD.population),
+//             color:  formatData.map(fD => fD.total),
+//             colorscale: 'Portland',
+//             opacity: 0.8},},];
+
+//     var bubbleLayout = {
+//         title: {
+//             text: `Overview of a country's per Capita CO2 emissions & GDP` ,
+//             font: {
+//                 family: 'Times New Roman',
+//                 size: 18,
+//                 color: 'dark gray',
+//                 },
+//         },
+//         autosize: true,
+//         responsive: true,
+//         xaxis: {
+//             automargin: true,
+//             title: {
+//                 text: 'Total CO2 Emissions per Capita',
+//                 font: {
+//                     family: 'Times New Roman',
+//                     size: 14,
+//                     color: 'dark gray',
+//                     },
+//             },
+//         },
+//         yaxis: { 
+//             automargin: true,
+//             title: {
+//                 text: 'GDP per Capita',
+//                 font: {
+//                     family: 'Times New Roman',
+//                     size: 14,
+//                     color: 'dark gray',
+//                     },
+//             },
+//         },};
+
+// Plotly.newPlot('bubbles', bubble, bubbleLayout)};
+
+//////////////-- SUMMARY TABLE ----//////////////////////////////////
+function createTable(formatData) {
+    
+    const table = document.querySelector("#myTable")
+
+    formatData.forEach(fD => {
+        const row = document.createElement('tr');
+        let td1 = document.createElement('td');
+        td1.innerText = fD.country;
+        let td2 = document.createElement('td');
+        td2.innerText = fD.total;
+        let td3 = document.createElement('td');
+        td3.innerText = fD.gdp;
+        let td4 = document.createElement('td');
+        td4.innerText = fD.population;
+        
+        row.appendChild(td1);
+        row.appendChild(td2);
+        row.appendChild(td3);
+        row.appendChild(td4);
+
+        table.appendChild(row);
+    })
+
+    console.log("testtable", formatData)
+};
+///////////////////////////////////////////////////////////////////////////////
+function plotAllVisuals () {
+    let formatData = cleanData();
+    plotBarChart(formatData);
+    plotBubbleChart(formatData);
+    createTable(formatData);
+    // plotSimpleBubble(formatData);
+}
+
 ////////////////////  FUNCTION - INITIALIZE DATA, EVENT HANDLER  //////////////////
 // function to read data, populate initial graphs
 function init() {
@@ -239,7 +269,7 @@ function init() {
                 .text(sample)
                 .property("value", sample);
         });
-        keymetrics()
+        plotAllVisuals()
     });
     
         // define event listener for drop down selection change on Year
@@ -250,7 +280,7 @@ function init() {
         // Fetch the JSON data and assign properties to global variables
                 d3.json(dataURL).then(function(data) {
                     rawData = data
-                    keymetrics()
+                    plotAllVisuals()
                 });
             });
 
@@ -262,7 +292,7 @@ function init() {
         // Fetch the JSON data and assign properties to global variables
                 d3.json(dataURL).then(function(data) {
                     rawData = data
-                    keymetrics()
+                    plotAllVisuals()
                 });
             });
         };
