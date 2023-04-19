9dde9889e4a444b8802b416515c0e0dcd0c98189
erica.b.graboyes <erica.b.graboyes@gmail.com>
2023-04-19 10:16:38 -0500

    added Daniela treemap

--- total.js
+++ total.js
@@ -1,128 +1,102 @@
-//set global variables
-let rawTotal = null;
+// Setting up year and continent selection
 let yrTotal = 2019
 let ccTotal = 'AF'
 
-////////////////////////////////////////////////////t//////////////////////
-function clean() {
-    // loop through data to generate list of countries for given year
-    const cnty = []
-    rawTotal.forEach(rD => {
-        if(!cnty.some(c => c === rD.Country)) {
-            cnty.push(rD.Country)
-        }
-    });
-    
-    // loop through data to generate list of categories for given year
-    const categories = []    
-    rawTotal.forEach(rD => {
-        if(!categories.some(c => c === rD.Category)) {
-            categories.push(rD.Category)
-        }
-    });
+//define constant arrays -- for drop down and to loop through
+// let dropDownYear = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+// let dropDownContinent = ["AF", "AS", "EU", "NA", "OC", "SA"]
+let rawData = null
 
-    const formatData = []
-    // Outer Loop iterates over countries / Inner Loop iterates over Category. for each country returns object with category and value
-    cnty.forEach ((c) => {
-        const cntyCategories = []
-        rawTotal.forEach((rD) => {
-            if (rD.Country === c) {
-                if (categories.some((cC) => {
-                    return cC === rD.Category
-                })) {
-                    // console.log('Country, Category, Value', `${rD['Country Code']}: ${rD.Category}: ${rD.Value}`);
-                    cntyCategories.push({
-                        category: rD.Category,
-                        value: rD.Value
-                    });
-                    }
-            };
-        })
-        
-        formatData.push({
-            country:c,
-            oil: cntyCategories.find(cC => cC.category === 'Oil').value,
-            coal: cntyCategories.find(cC =>cC.category === 'Coal').value, 
-            gas: cntyCategories.find(cC => cC.category === 'Gas').value,
-            cement: cntyCategories.find(cC => cC.category === 'Cement').value, 
-            flaring: cntyCategories.find(cC => cC.category === 'Flaring').value, 
-            other: cntyCategories.find(cC => cC.category === 'Other').value,
-            total: cntyCategories.find(cC => cC.category === 'Total').value,
-            // gdp: cntyCategories.find(cC => cC.category === 'GDP per capita (current US$)').value,
-            population: cntyCategories.find(cC => cC.category === 'Population, total').value/1000000,
-        });
-    });
-    
-    // Sort bargraph data by total emissions for stacked bar chart 
-    formatData.sort((d1, d2) => {
-        return d2.total - d1.total});
-
-    return formatData
+// define chart options for tree map
+var chartOptions = {
+  series: [
+    {
+      data: []
+    }
+  ],
+  chart: {
+    type: 'treemap',
+    height: 500,
+  },
+  title: {
+    text: 'Countries by Total CO2 Emissions',
+  },
 };
-///////////////////--SIMPLE BAR CHART IN APEX--///////////////////////////////
-function plotTreeMap (formatData) {
-    let treeChart = formatData.map(tC => {
-        return {
-            x: tC.country,
-            y: tC.total
-        }
-    })
-    console.log(treeChart);
 
-    var options = {
-        series: [{data: treeChart}],
+let chart = new ApexCharts(document.querySelector('#treemap'), chartOptions);
+chart.render();
 
-        legend: {show: false},
+// updateChart for total CO2 Emission
+function updateChart() {
+  // console.log(rawData)
+  let filteredData = rawData.filter(d => d['Category'] == 'Total');
+  
+  let chartData = filteredData.map(c => ({
+    x: c['Country'], y: Math.floor(c.Value)}))
+  
+  chart.updateSeries([{
+    data: chartData
+  }]);
+  console.log(chartData)
 
-        chart: {
-            height: 350,
-            type: 'treemap'},
-
-        title: {
-            text: 'Basic Treemap'
-        }
-    };
-
-    const tree = document.querySelector("#tree");
-
-    tree.innerHTML = '';
-    
-    var chart = new ApexCharts(document.querySelector("#tree"), options);
-    chart.render();
-}
-//////////////// FUNCTION -- CALL PLOTS FOR VISUALIZATION --- ////////////////
-function plot() {
-    let formatData = clean();
-    plotTreeMap(formatData);
 }
 
-////////////////////  FUNCTION - INITIALIZE DATA, EVENT HANDLER  //////////////////
+/////// SELECT YEAR //////
+// let yearSelect = document.querySelector('#selYr')
+// let continentSelect = document.querySelector('#selCC')
+
+// yearSelect.addEventListener('change', (event) => {yrTotal = parseInt(event.target.value)
+//   updateChart()
+// })
+
+// continentSelect.addEventListener('change', (event) => {
+//     ccTotal = event.target.value
+//   updateChart()
+// })
+
+///////FUNCTION - INITIALIZE DATA, EVENT HANDLER  ///////
+
 // function to read data, populate initial graphs
-function setup() {
-    const startTotal = `http://127.0.0.1:5000/api/v1/continent_totals/${ccTotal}?year=${yrTotal}`
-    
-    // Fetch JSON data to load initial charts
-    d3.json(startTotal).then(function(data) {
-        rawTotal = data
-        console.log("rawDataTotal", rawTotal)
-        plot()
+function init() {
+    var totalURL = `http://127.0.0.1:5000/api/v1/continent_totals/${ccTotal}?year=${yrTotal}`
+  
+  // Fetch the JSON data and assign properties to global variables
+    d3.json(totalURL).then(function(data) {
+        rawData = data
+
+    //   dropDownYear.reverse()
+    //   dropDownYear.forEach((sample) => {
+    //       selectYr
+    //           .append("option")
+    //           .text(sample)
+    //           .property("value", sample);
+    //   });
+
+    //   // append Continent Code to drop down selector
+    //   dropDownContinent.forEach((sample) => {
+    //       selectCC
+    //           .append("option")
+    //           .text(sample)
+    //           .property("value", sample);
+    //   });
+        updateChart(rawData);
     });
-    
     // define event listener for drop down selection change on Year
     selectYr.on('change', function() {
         yrTotal = selectYr.property("value")
-        const changeTotal = `http://127.0.0.1:5000/api/v1/continent_totals/${ccTotal}?year=${yrTotal}`
+        const tChange = `http://127.0.0.1:5000/api/v1/continent_totals/${ccTotal}?year=${yrTotal}`
 
-        // Fetch the JSON data and refresh charts using Total API
-        d3.json(changeTotal).then(function(data) {
-            rawTotal = data
-            plot()
+      // Fetch the JSON data and refresh charts using Total API
+        d3.json(tChange).then(function(data) {
+            rawData = data
+            console.log("YearSelection", rawData)
+            updateChart()
         });
 
         yrPC = selectYr.property("value")
         const changePC = `http://127.0.0.1:5000/api/v1/continent_per_capita/${ccPC}?year=${yrPC}`
-
-       // Fetch the JSON data and refresh charts using Per Cap API
+    
+        // Fetch the JSON data and refresh charts using Per Cap API
         d3.json(changePC).then(function(data) {
             rawPC = data
             plotAllVisuals()
@@ -131,13 +105,14 @@ function setup() {
 
     // define event listener for drop down selection change on Continent Code
     selectCC.on('change', function() {
-        ccTotal = selectCC.property("value")            
-        const changeTotal = `http://127.0.0.1:5000/api/v1/continent_totals/${ccTotal}?year=${yrTotal}`
+        ccTotal = selectCC.property("value")
+        const tChange = `http://127.0.0.1:5000/api/v1/continent_totals/${ccTotal}?year=${yrTotal}`
 
-        // Fetch the JSON data and refresh charts using Total API
-        d3.json(changeTotal).then(function(data) {
-            rawTotal = data
-            plot()
+      // Fetch the JSON data and assign properties to global variables
+        d3.json(tChange).then(function(data) {
+            rawData = data
+            console.log("YearSelection", rawData)
+            updateChart()
         });
 
         ccPC = selectCC.property("value")            
@@ -148,7 +123,7 @@ function setup() {
             rawPC = data
             plotAllVisuals()
         });
-    });
+     });
 };
 
-setup();
+init();
