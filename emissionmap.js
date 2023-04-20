829c428a0156ef715239c5b376cedc8e7223ecbb
erica.b.graboyes <erica.b.graboyes@gmail.com>
2023-04-20 11:44:55 -0500

    replaced marker with layergroup, added circles

--- emissionmap.js
+++ emissionmap.js
@@ -1,45 +1,39 @@
-let rawMap = null
+
 let mapYr = 2019
 
 const geoData = coords 
 
-function createMap(totalEmissions) {
+// Create the tile layer that will be the background of our map.
+let base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+  
+// Create a baseMaps object to hold the streetmap layer.
+let baseMaps = {"base map": base};
 
-    // Create the tile layer that will be the background of our map.
-    let base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-  
-  
-    // Create a baseMaps object to hold the streetmap layer.
-    let baseMaps = {
-      "base map": base
-    };
-  
-    // Create an overlayMaps object to hold the bikeStations layer.
-    let overlayMaps = {
-      "CO2 Emissions": totalEmissions
-    };
-  
-    // Create the map object with options.
-    let map = L.map("map", {
-      center: [40.73, -74.0059],
-      zoom: 3,
-      layers: [base, totalEmissions]
-    });
-  
-    // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(map);
-  }
-  
-  function createMarkers(response) {
-  
+// define new layergroup
+var totalEmissions = new L.LayerGroup();
+
+// Create an overlayMaps object to hold the bikeStations layer.
+let overlayMaps = {"CO2 Emissions": totalEmissions};
+
+// Create the map object with options.
+let map = L.map("map", {
+    center: [40.73, -74.0059],
+    zoom: 3,
+    layers: [base, totalEmissions]
+});
+
+// Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+}).addTo(map);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+function createMarkers(response) {
     // Pull the "stations" property from response.data.
     let rawMap = response
 
-      
     const cnty = []
     rawMap.forEach(rM => {
         if(!cnty.some(c => c === rM.Country)) {
@@ -90,25 +84,25 @@ function createMap(totalEmissions) {
 
     console.log("Test LatLong", mapData)
 
-    let emissionsMarkers = [];
-
     // Loop through the array.
     for (let index = 0; index < mapData.length; index++) {
         let mapD = mapData[index];
 
-        let emissionMarker = L.marker([mapD.lat, mapD.lon])
-        .bindPopup("<h5>" + mapD.country + "<h5><h5>Total CO2 Emissions: " + mapD.total + "</h5>");
+        L.circleMarker([mapD.lat, mapD.lon],{
+                fillOpacity:1,
+                clickable: true,
+                stroke: true,
+                color: "black",
+                weight: 0.25,
+})
+        .bindPopup("<h5>" + mapD.country + "<h5><h5>Total CO2 Emissions: " + mapD.total + "</h5>").addTo(totalEmissions)
+    };
 
-        emissionsMarkers.push(emissionMarker)
-
-        console.log(emissionsMarkers)}
-
-        createMap(L.layerGroup(emissionsMarkers));
-
-  }
+    totalEmissions.addTo(map);
+};
   
-  // Perform an API call to API to get the emissions information. Call createMarkers when it completes.
-  const startMap = `http://127.0.0.1:5000/api/v1/countries_totals?year=${mapYr}`
+// Perform an API call to API to get the emissions information. Call createMarkers when it completes.
+const startMap = `http://127.0.0.1:5000/api/v1/countries_totals?year=${mapYr}`
   
-  d3.json(startMap).then(createMarkers);
+d3.json(startMap).then(createMarkers);
   
